f6e899c97bff80679a2585208929ddd8be3b7ce5
Punit <spunit2025@gmail.com>
2024-08-29 22:34:32 +0530

    added an extra variable to make it a program to add 3 numbers

--- demo.c
+++ demo.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    int num1, num2, sum;
+    int num1, num2, num3, sum;
 
     printf("Enter first number: ");
     scanf("%d", &num1);
@@ -9,7 +9,10 @@ int main() {
     printf("Enter second number: ");
     scanf("%d", &num2);
 
-    sum = num1 + num2;
+    printf("Enter third number: ");
+    scanf("%d", &num3);
+
+    sum = num1 + num2 + num3;
     printf("Sum: %d\n", sum);
 
     return 0;
